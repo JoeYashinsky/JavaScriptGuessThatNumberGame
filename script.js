153ec3f4c94279b10bebc3e12c589a04f84dcd25
Your Name <you@example.com>
2021-02-24 18:30:25 -0500

    .message .number .score and .guess elements all work properly when manipulated within the script.js file

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
