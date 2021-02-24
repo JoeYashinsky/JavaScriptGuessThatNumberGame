a1b44c5972020a8368ab0ccfc5db2181894973e1
Your Name <you@example.com>
2021-02-24 17:43:01 -0500

    set .number and .score elements equal to integers  to make sure querySelector works properly

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
