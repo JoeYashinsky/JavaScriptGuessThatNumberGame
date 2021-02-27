d68139f2dae0b1a9c578582f045242ee7bed3bc5
Your Name <joey.yashinsky@gmail.com>
2021-02-27 14:22:43 -0500

    Game is now fully functional with all features and css stylings

--- script.js
+++ script.js
@@ -77,6 +77,6 @@ Implement game restart functionality so that player can make a new guess! Here's
 
 1. Select the element with the 'again' class and attach a click event handler.
 2. In the handler function, restore initial values of the score and number variables.
-3. Restore the initial conditions of the message, number, score and guess input field.
+3. Restore the initial conditions of the message, number, score and guess input field
 4. Also restore the original background color (#222) and number width (15rem).
 */
