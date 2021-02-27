6344b634bb34d67931e666d090242c1cd59c5845
Your Name <joey.yashinsky@gmail.com>
2021-02-27 11:06:04 -0500

    highscore now two words (high score) and secret number correctly not shown until player accurately figures it out

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -56,3 +55,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+///////////////////////////
+// Coding Challenge #1
+
+/*
+Implement a game rest functionality so that the player can make a new guess! Here's how:
+
+1. Select the element with the 'again' class and attach a click event handler.
+2. In the handler function, restore initial values of the score and number variables.
+3. Restore the initial conditions of the message, number, score and guess input field.
+4. Also restore the original background color (#222) and number width (15rem).
+*/
